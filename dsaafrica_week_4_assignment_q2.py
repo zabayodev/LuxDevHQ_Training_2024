1285f440502c4bef852a4c8c1d7600562ade10cb
Emmanuel M <apple@Emmanuel-M.local>
2024-08-28 15:19:52 +0200

    Week 4 assignment in the bootcamp, question 2

---
+++ dsaafrica_week_4_assignment_q2.py
@@ -0,0 +1,17 @@
+'''
+List comprehension is a concise way to create lists in Python. It provides a more readable and compact syntax for
+generating lists compared to traditional methods like loops. 
+
+'''
+# Creating a list comprehension expression
+'''
+[expression for item in iterable if condition]
+'''
+#expression: The value to include in the new list.
+#item: The variable representing each element in the iterable.
+#iterable: The collection to loop over such as a list and a range.
+#condition which is optional is a filter that determines if an item should be included in the new list.
+
+
+
+
