a26e9340cc224536e6ba5ce4d2a42eebdf97a787
Emmanuel M <apple@Emmanuel-M.local>
2024-08-28 14:34:41 +0200

    Week 4 assignment in the bootcamp

--- dsaafrica_week_4_assignment.py
+++ dsaafrica_week_4_assignment.py
@@ -3,3 +3,30 @@ A palindrome is a sequence of characters that reads the same forward and backwor
 the reverse of the word looks like the original word.
 
 '''
+class Stack:
+    def __init__(self):
+        self.items = []
+    
+    def is_empty(self):
+        return self.items == []
+    
+    def push(self, item):
+        self.items.append(item)
+    
+    def pop(self):
+        return self.items.pop()
+    
+    def size(self):
+        return len(self.items)
+    
+    def palindrome_check(input_string):
+        stack = Stack()
+        cleaned_string = input_string.replace(" ", "").lower()
+        for char in cleaned_string:
+            stack.push(char)
+        
+        reversed_string = ''
+        while not stack.is_empty():
+            reversed_string += stack.pop()
+        
+        return cleaned_string == reversed_string
