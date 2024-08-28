a39391e366277cd37e3b07f748d79a04efedf4ef
Emmanuel M <apple@Emmanuel-M.local>
2024-08-28 15:55:32 +0200

    Week 4 assignment in the bootcamp, question 3 and examples

---
+++ dsaafrica_week_4_assignment_q3.py
@@ -0,0 +1,5 @@
+'''
+A compound data type in Python is a data structure that can hold multiple values, often of different types, 
+within a single object. These data types allow you to group together a related data and work with them as
+a unit. The main compound data types in Python are lists, tuples, dictionaries and sets.
+'''
